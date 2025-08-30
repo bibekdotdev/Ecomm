6fa5fdf15c634f2510467df681a46604f2ce793c
Bibek Jana <168276117+bibekdotdev@users.noreply.github.com>
2025-08-30 10:32:41 +0530

    Update addtocart.js

--- server/models/addtocart.js
+++ server/models/addtocart.js
@@ -4,20 +4,20 @@ const addtocartSchema = new mongoose.Schema(
   {
     buyer: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Owner", // Assuming you have a separate 'Owner' model for the buyer
+      ref: "Owner", 
       required: true,
     },
     orderItems: 
       {
         product: {
           type: mongoose.Schema.Types.ObjectId,
-          ref: "Product", // Assuming you have a 'Product' model
+          ref: "Product",
           required: true,
         },
         quantity: {
           type: Number,
           required: true,
-          min: 1, // Minimum quantity of 1
+          min: 1,
         },
       },
     
